683fedcfb68f870aed37167d5f5ab47002a3e38f
hm-imu-sc <hmimusc850@gmail.com>
2022-05-22 10:37:44 +0600

    passenger authetication complete git add .!

--- main/urls.py
+++ main/urls.py
@@ -7,6 +7,8 @@ urlpatterns = [
     path("", views.HomePage.as_view(), name="home_page"),
     path("passenger_registration_page", views.PassengerRegistrationPage.as_view(), name="passenger_registration_page"),
     path("passenger_registration", views.PassengerRegistration.as_view(), name="passenger_registration"),
-    path("login_page/", views.LoginPage.as_view(), name="login_page"),
+    path("login_page", views.LoginPage.as_view(logout_required=True), name="login_page"),
+    path("login", views.Login.as_view(logout_required=True), name="login"),
+    path("logout", views.Logout.as_view(), name="logout"),
     path("add_station", views.addStation, name="add_station"),
 ]
--- main/views.py
+++ main/views.py
@@ -1,7 +1,8 @@
 
 from my_modules.base_views import TemplateContextView, NoTemplateView
-from main.models import Station, Passenger
+from main.models import Station, Passenger, Admin
 from django.shortcuts import render,redirect
+from datetime import datetime, timedelta
 from hashlib import sha256
 
 class HomePage(TemplateContextView):
@@ -22,21 +23,98 @@ class PassengerRegistration(NoTemplateView):
         email = request.POST.get("email")
         password = request.POST.get("password")
 
-        Passenger.objects.create(
-            nid         = nid,
-            name        = name,
-            email       = email,
-            username    = username,
-            password    = sha256(password.encode("ASCII")).hexdigest()
-        )
+        try:
+            Passenger.objects.create(
+                nid         = nid,
+                name        = name,
+                email       = email,
+                username    = username,
+                password    = sha256(password.encode("ASCII")).hexdigest()
+            )
+        except:
+            return
 
     def get_redirection(self):
         return 'main:passenger_registration_page'
 
 
 class LoginPage(TemplateContextView):
+    
+    def get_context(self, request, *args, **kwargs):
+        return {
+            'session': {
+                'login_error': ''
+            }, 
+            'old_info': {
+                'username': '',
+                'password': ''
+            }
+        }
+
     def get_template(self):
         return 'login_page.html'
+
+
+class Login(NoTemplateView):
+    
+    def act(self, request, *args, **kwargs):
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        domain = None
+
+        user = list(Passenger.objects.filter(username__exact=username))
+
+        if len(user) == 1:
+            user = user[0]
+            domain = "passenger"
+        else:
+            user = list(Admin.objects.filter(username__exact=username))        
+            if len(user) == 1:
+                user = user[0]
+                domain = "admin"
+
+        if domain is not None:
+            if self.__veryfy_password(user, password):
+                request.session['user'] = {
+                    'login_status': True,
+                    'username': username,
+                    'domain': domain
+                }
+                self.redirect_to = 'main:home_page'
+                request.session['login_error'] = ''
+            else:
+                self.redirect_to = 'main:login_page'
+                request.session['login_error'] = 'error'
+                request.session['old_info'] = {
+                    'username': username,
+                    'password': password
+                }
+        else:
+            self.redirect_to = 'main:login_page'
+            request.session['login_error'] = 'error'
+            request.session['old_info'] = {
+                'username': username,
+                'password': password
+            }
+
+    def get_redirection(self):
+        return self.redirect_to
+
+    def __veryfy_password(self, user, password):
+        return sha256(password.encode('ascii')).hexdigest() == user.password        
+
+
+class Logout(NoTemplateView):
+    def act(self, request, *args, **kwargs):
+        request.session['user'] = {'login_status': False}
+        try:
+            del request.session['login_error']
+            del request.session['old_info']
+        except KeyError:
+            return
+
+    def get_redirection(self):
+        return 'main:login_page'
 
 
 class AddStationPage(TemplateContextView):
